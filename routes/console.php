b265131fe15314d91e525f9c4b845f2f701101e9
04Amitk <am.amit.kumar@2001@gmail.com>
2026-08-04 18:37:59 +0530

    Update PHP version requirement to ^8.3 in composer files and format console commands

--- routes/console.php
+++ routes/console.php
@@ -4,5 +4,8 @@ use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 
 Artisan::command('inspire', function () {
-    $this->comment(Inspiring::quote());
+  $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+Schedule::command('queue:listen --max-jobs=50 --max-time=57 --tries=1')->everyMinute()->withoutOverlapping(5);
+Schedule::command('queue:prune-failed --hours=48')->daily();
